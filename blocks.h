2fe91b34f39f8b2f740d05dc03b87801697a8e5b
vulf <pranavsivvam@gmail.com>
2020-10-09 11:14:36 +0530

    clock: changed update time to 1 sec

--- blocks.h
+++ blocks.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "~/scripts/dwmblocks/clock",					30,		12},
+	{"", "~/scripts/dwmblocks/clock",					1,		12},
 	
 
 	{"", "~/scripts/dwmblocks/volume",						0,		10},
